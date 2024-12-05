f4c595b90115880c014016bb40f26596f7af1a97
Funsho <funshoajayi29@gmail.com>
2024-12-06 00:39:11 +0100

    updated landing page hero section with new design

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-bg": "url('/assets/images/hero-bg.png')",
+      },
+    },
+  },
+  plugins: [],
+};
